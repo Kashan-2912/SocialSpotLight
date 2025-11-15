9f19f577b28301be7a3bf1daa3eea864a35d79ce
Kashan-2912 <mkashan2912@gmail.com>
2025-11-15 20:00:53 +0500

    feat: UI Revamp (login, signup page)

---
+++ client/src/components/SignInComponent.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/ui/sign-in-card-2";
+
+export const SignInComponent = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <Component />
+    </div>
+  );
+};
---
+++ client/src/components/SignUpComponent.tsx
@@ -0,0 +1,9 @@
+import { Component } from "@/components/ui/sign-up-card-2";
+
+export const SignUpComponent = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <Component />
+    </div>
+  );
+};
